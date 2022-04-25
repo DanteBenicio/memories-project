def479179c9f15d9a1a05540cab1a416399e805d
Dante Benicio <dantecosta79@gmail.com>
2022-04-25 15:58:39 -0300

    created createPost and updatePost route

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,3 +4,5 @@ import axios from 'axios';
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchPosts = () => axios.get('/posts');
+export const createPost = (newPost: any) => axios.post('/posts', newPost);
+export const updatePost = (id: string, updatedPost: any) => axios.patch(`/posts/${id}`, updatedPost);
