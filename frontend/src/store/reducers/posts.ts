45b426e6cd480f87156c07cd0881585cc5a7ee62
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 18:52:44 -0300

    create UPDATE, LIKE and DELETE case

--- frontend/src/store/reducers/posts.ts
+++ frontend/src/store/reducers/posts.ts
@@ -7,7 +7,14 @@ export default (posts = [], action: any) => {
       return action.payload;
 
     case CREATE:
-      return posts;
+      return [...posts, action.payload];
+
+    case UPDATE:
+    case LIKE:
+      return posts.map(post => (post._id === action.payload._id ? action.payload : post));
+
+    case DELETE:
+      return posts.filter(post => post._id !== action.payload.id);
 
     default:
       return posts;
