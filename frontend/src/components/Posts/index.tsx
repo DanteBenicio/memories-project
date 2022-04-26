89be3ec32d6f1ba8e07eaceb2bfc7f6be7ddc38f
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 19:37:08 -0300

    changed className

--- frontend/src/components/Posts/index.tsx
+++ frontend/src/components/Posts/index.tsx
@@ -11,7 +11,7 @@ export default function Posts() {
 
   return (
     !posts.length ? <CircularProgress /> : (
-      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+      <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
         {posts.map(post => (
           <Grid key={post._id} item xs={12} sm={6}>
             <Post post={post} />
