8c16f0f1f7fa2c0ef7f025d5a3dcfb862735359e
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 18:49:21 -0300

    create deletePost and likePost api request

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -6,3 +6,5 @@ axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 export const fetchPosts = () => axios.get('/posts');
 export const createPost = (newPost: any) => axios.post('/posts', newPost);
 export const updatePost = (id: string, updatedPost: any) => axios.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id: string) => axios.delete(`/posts/${id}`);
+export const likePost = (id: string, post: any) => axios.patch(`/posts/${id}/likePost`, post);
