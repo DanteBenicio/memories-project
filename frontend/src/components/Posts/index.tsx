f24d5f311de254e2305c60156f65f1caec343dc0
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 19:36:37 -0300

    remove console.log

--- frontend/src/components/Posts/index.tsx
+++ frontend/src/components/Posts/index.tsx
@@ -9,8 +9,6 @@ export default function Posts() {
   const posts = useSelector((state: any) => state.posts);
   const classes = useStyles();
 
-  console.log(posts);
-
   return (
     !posts.length ? <CircularProgress /> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
