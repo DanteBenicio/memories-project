71c6114439d4494c80dbf9f08a9b358d0cff8ef4
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 19:36:26 -0300

    import Post, PostsProps and IState

--- frontend/src/components/Posts/index.tsx
+++ frontend/src/components/Posts/index.tsx
@@ -1,6 +1,9 @@
 import { useSelector } from 'react-redux';
 import { CircularProgress, Grid } from '@material-ui/core';
 import useStyles from './styles';
+import Post from './Post';
+import { PostsProps } from '../../types/Posts';
+import { IState } from '../../types/State';
 
 export default function Posts() {
   const posts = useSelector((state: any) => state.posts);
