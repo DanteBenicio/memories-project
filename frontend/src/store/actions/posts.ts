2dd9a527a5cb24a12ef85e808c3f373656a66c6c
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 18:51:02 -0300

    actions

--- frontend/src/store/actions/posts.ts
+++ frontend/src/store/actions/posts.ts
@@ -4,9 +4,49 @@ import * as api from '../../api';
 
 export const getPosts = () => async (dispatch: Dispatch) => {
   try {
-    const { data } = await api.fetchPosts(); // pegar posts
+    const { data } = await api.fetchPosts();
 
-    dispatch({ type: 'FETCH_ALL', payload: data }); // dispara a action FETCH_ALL e passa um payload
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createPost = (post: any) => async (dispatch: Dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updatePost = (id: string, post: any) => async (dispatch: Dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = (id: string) => async (dispatch: Dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = (id: string, post: any) => async (dispatch: Dispatch) => {
+  try {
+    const { data } = await api.likePost(id, post);
+
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.error(error);
   }
