80c31991eecdd4ce4f2ace7ffc484339519e90a0
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 21:10:15 -0300

    import express and created PORT instance

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,4 @@
-import express, {Router, Request, Response} from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from 'express';
 import cors from 'cors';
 import { connectToMongoDB } from './services/mongoose';
 import dotenv from 'dotenv';
@@ -10,7 +8,7 @@ dotenv.config()
 
 const app = express();
 const route = express.Router()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors())
 app.use(express.json({ limit: "30mb" }));
