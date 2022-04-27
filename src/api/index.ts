8235dd50cae1e3b1db6bd4f108c8d555674225b3
Dante Benicio <dantecosta79@gmail.com>
2022-04-27 16:42:15 -0300

    get BACKEND_URL env variable

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = process.env.BACKEND_URL;
 
 export const fetchPosts = () => axios.get('/posts');
 export const createPost = (newPost: any) => axios.post('/posts', newPost);
