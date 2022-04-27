2c6fe2731ee38fccf7109109ce1215d31f22bc48
Dante Benicio <dantecosta79@gmail.com>
2022-04-27 17:04:21 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-<<<<<<< HEAD
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-=======
-axios.defaults.baseURL = process.env.BACKEND_URL;
->>>>>>> 8235dd50cae1e3b1db6bd4f108c8d555674225b3
 
 export const fetchPosts = () => axios.get('/posts');
 export const createPost = (newPost: any) => axios.post('/posts', newPost);
