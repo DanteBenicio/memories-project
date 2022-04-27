c249174f921b8679ab926ef697ee15a4c86dadb5
Dante Benicio <dantecosta79@gmail.com>
2022-04-26 21:07:15 -0300

    removed bodyParser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,8 +11,9 @@ const app = express();
 const route = express.Router()
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json({ limit: "30mb" }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors())
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true}))
 app.use(route)
 app.use('/posts', postRoutes)
 app.use(cors())
